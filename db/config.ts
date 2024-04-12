d342f5b5e66843b6c3fe7584b769abee1a083448
Nick Dubelman <ndubelman@gmail.com>
2024-04-12 08:43:00 -0700

    add more db entities

--- db/config.ts
+++ db/config.ts
@@ -32,11 +32,37 @@ const NBAPlayer = defineTable({
   },
 })
 
+const NBAGame = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    date: column.date(),
+    time: column.text(),
+    period: column.number(),
+    status: column.text(),
+    homeTeamScore: column.number(),
+    awayTeamScore: column.number(),
+    homeTeamId: column.number({ references: () => NBATeam.columns.id }),
+    awayTeamId: column.number({ references: () => NBATeam.columns.id }),
+  },
+})
+
+const NBAPlayerGameStats = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    points: column.number(),
+    minutes: column.number(),
+    playerId: column.number({ references: () => NBAPlayer.columns.id }),
+    gameId: column.number({ references: () => NBAGame.columns.id }),
+  },
+})
+
 // https://astro.build/db/config
 export default defineDb({
   tables: {
     Participant,
     NBATeam,
     NBAPlayer,
+    NBAGame,
+    NBAPlayerGameStats,
   },
 })
--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,11 @@
-import { NBAPlayer, NBATeam, Participant, db } from 'astro:db'
+import {
+  db,
+  NBAGame,
+  NBAPlayer,
+  NBAPlayerGameStats,
+  NBATeam,
+  Participant,
+} from 'astro:db'
 
 // https://astro.build/db/seed
 export default async function seed() {
@@ -22,5 +29,38 @@ export default async function seed() {
     { name: 'Nikola Jokic', teamId: 2, participantId: 2 },
     { name: 'Jamal Murray', teamId: 2, participantId: 3 },
     { name: 'Michael Porter Jr.', teamId: 2 },
+  ])
+
+  // Log some NBA games
+  await db.insert(NBAGame).values([
+    {
+      date: new Date('2024-04-12'),
+      time: '',
+      period: 4,
+      status: 'Final',
+      homeTeamScore: 100,
+      awayTeamScore: 95,
+      homeTeamId: 1,
+      awayTeamId: 2,
+    },
+    {
+      date: new Date('2024-04-12'),
+      time: '',
+      period: 4,
+      status: 'Final',
+      homeTeamScore: 105,
+      awayTeamScore: 110,
+      homeTeamId: 2,
+      awayTeamId: 1,
+    },
+  ])
+
+  // Log some player stats for the games
+  await db.insert(NBAPlayerGameStats).values([
+    { playerId: 1, gameId: 1, points: 30, minutes: 40 },
+    { playerId: 2, gameId: 1, points: 20, minutes: 35 },
+    { playerId: 3, gameId: 2, points: 25, minutes: 40 },
+    { playerId: 4, gameId: 2, points: 15, minutes: 30 },
+    { playerId: 5, gameId: 2, points: 20, minutes: 35 },
   ])
 }
