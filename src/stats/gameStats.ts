d0e72482f4e3be44432e22a68be6532b6cd57015
Nick Dubelman <ndubelman@gmail.com>
2024-04-14 10:50:28 -0700

    add TopScorersOverall and getGameStats

---
+++ src/stats/gameStats.ts
@@ -0,0 +1,24 @@
+import {
+  db,
+  eq,
+  NBAPlayer,
+  NBAPlayerGameStats,
+  NBATeam,
+  Participant,
+} from 'astro:db'
+
+export default async function getGameStats() {
+  const stats = await db
+    .select()
+    .from(NBAPlayerGameStats)
+    .innerJoin(NBAPlayer, eq(NBAPlayer.id, NBAPlayerGameStats.playerId))
+    .innerJoin(NBATeam, eq(NBATeam.id, NBAPlayer.teamId))
+    .leftJoin(Participant, eq(Participant.id, NBAPlayer.participantId))
+
+  return stats.map((stat) => ({
+    player: stat.NBAPlayer.name,
+    team: stat.NBATeam.shortName,
+    points: stat.NBAPlayerGameStats.points,
+    participant: stat.Participant?.name || null,
+  }))
+}
