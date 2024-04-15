b53abb6e1ee1cdcc4b746050dfb9b341fc06d001
Nick Dubelman <ndubelman@gmail.com>
2024-04-15 07:16:19 -0700

    switch from vercel to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 import db from '@astrojs/db'
 import svelte from '@astrojs/svelte'
-import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db(), svelte()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 })
