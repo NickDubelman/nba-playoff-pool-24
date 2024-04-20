dc72240c71dcb1bf3a99edad371811fbbd30f7f9
Nick Dubelman <ndubelman@gmail.com>
2024-04-19 20:31:10 -0700

    Update eliminatedTeams.ts
    Eliminate Bulls

--- src/eliminatedTeams.ts
+++ src/eliminatedTeams.ts
@@ -1,4 +1,8 @@
 // Hardcoded list of eliminated teams
-const eliminatedTeams: string[] = ['GSW', 'ATL']
+const eliminatedTeams: string[] = [
+  'GSW',
+  'ATL',
+  'CHI',
+]
 
 export default eliminatedTeams
